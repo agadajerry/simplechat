561c1757a4d652b8db16dee14eeb0c467b6866b6
Idoko Agada Jerry <Idokoidoko4@gmail.com>
2023-11-23 11:03:30 +0100

    chat widget

--- chat-library.js
+++ chat-library.js
@@ -1,41 +1,16 @@
-// chat-library.js
+const socket = io();
 
-var SimpleChat = (function() {
-    var chatOutput, messageInput;
-    var userBackgroundColor = '#ffffff';
+document.getElementById('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const message = document.getElementById('m').value;
+    socket.emit('chat message', message);
+    document.getElementById('m').value = '';
+    return false;
+});
 
-    function appendMessage(message) {
-        var messageElement = document.createElement("div");
-        messageElement.textContent = message;
-        messageElement.style.backgroundColor = userBackgroundColor; // Set background color
-        chatOutput.appendChild(messageElement);
-
-        chatOutput.scrollTop = chatOutput.scrollHeight;
-    }
-
-    return {
-        initialize: function(outputId, inputId) {
-            chatOutput = document.getElementById(outputId);
-            messageInput = document.getElementById(inputId);
-        },
-
-        setChatBackgroundColor: function(color) {
-            userBackgroundColor = color;
-        },
-
-        sendMessage: function() {
-            var message = messageInput.value;
-
-            if (message.trim() !== "") {
-                appendMessage("You: " + message);
-
-                setTimeout(function() {
-                    var response = "Bot: Thanks for your message!";
-                    appendMessage(response);
-                }, 500);
-
-                messageInput.value = "";
-            }
-        },
-    };
-})();
+socket.on('chat message', function(msg) {
+    const messages = document.getElementById('messages');
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(msg));
+    messages.appendChild(li);
+});
---
+++ index/embed.js
@@ -0,0 +1,58 @@
+(function(d, t, userId) {
+    var BASE_URL = "https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.0.1/socket.io.js"; // Replace with the actual URL of your Socket.IO server
+    var g = d.createElement(t),
+        s = d.getElementsByTagName(t)[0];
+    g.src = BASE_URL;
+    g.defer = true;
+    g.async = true;
+    s.parentNode.insertBefore(g, s);
+    g.onload = function() {
+        var socket = io("https://whatsapp-bot-go3u.onrender.com");
+
+        var chatContainer = document.createElement('div');
+        chatContainer.id = 'chat-container';
+        chatContainer.innerHTML = `
+            <ul id="messages"></ul>
+            <form id="form" action="">
+                <input id="m" autocomplete="off" />
+                <button>Send</button>
+            </form>
+        `;
+
+        document.body.appendChild(chatContainer);
+
+        // Code to handle user input and display messages
+        const form = document.getElementById('form');
+        const input = document.getElementById('m');
+        const messages = document.getElementById('messages');
+
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            const message = input.value.trim();
+
+            if (message !== '') {
+                // Send the message to the server
+                socket.emit('sendMessage', message);
+
+                // Clear the input field
+                input.value = '';
+            }
+
+            return false;
+        });
+
+        socket.on('chat-message', function(msg) {
+            // Display incoming messages in the chat
+            console.log(msg)
+            const li = document.createElement('li');
+            li.appendChild(document.createTextNode(msg.message));
+            messages.appendChild(li);
+        });
+
+
+        // Your additional code to interact with the socket and use the userId
+        socket.emit('setUserId', userId);
+
+        // Rest of your code remains the same...
+    };
+})(document, "script", "uniqueUserId");
