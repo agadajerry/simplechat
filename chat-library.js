b3e6ee2abee2ab3dffe5c3a0518eda5c46fd0cb2
Idoko Agada Jerry <Idokoidoko4@gmail.com>
2023-11-23 22:04:55 +0100

    chat widget

--- chat-library.js
+++ chat-library.js
@@ -1,16 +1,32 @@
-const socket = io();
+const dynamicText = document.querySelector("h1 span");
+const words = ["Love", "like Art", "the Future", "Everything", "Jerry"];
 
-document.getElementById('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const message = document.getElementById('m').value;
-    socket.emit('chat message', message);
-    document.getElementById('m').value = '';
-    return false;
-});
+// Variables to track the position and deletion status of the word
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
 
-socket.on('chat message', function(msg) {
-    const messages = document.getElementById('messages');
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(msg));
-    messages.appendChild(li);
-});
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0, charIndex);
+    dynamicText.textContent = currentChar;
+    dynamicText.classList.add("stop-blinking");
+
+    if (!isDeleting && charIndex < currentWord.length) {
+        // If condition is true, type the next character
+        charIndex++;
+        setTimeout(typeEffect, 200);
+    } else if (isDeleting && charIndex > 0) {
+        // If condition is true, remove the previous character
+        charIndex--;
+        setTimeout(typeEffect, 100);
+    } else {
+        // If word is deleted then switch to the next word
+        isDeleting = !isDeleting;
+        dynamicText.classList.remove("stop-blinking");
+        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+        setTimeout(typeEffect, 1200);
+    }
+}
+
+typeEffect();
--- index/embed.js
+++ index/embed.js
@@ -1,97 +1,131 @@
 (function(d, t, userId) {
-    var BASE_URL = "https://whatsapp-bot-go3u.onrender.com";
-    var isToggle = false;
-    const BOT_WAITING = `Bot is processing your request. Please wait...`
-    var g = d.createElement(t),
-        s = d.getElementsByTagName(t)[0];
-    g.src = BASE_URL + "/socket.io/socket.io.js";
-    g.defer = true;
-    g.async = true;
-    s.parentNode.insertBefore(g, s);
-    g.onload = function() {
-        var socket = io(BASE_URL);
+        var BASE_URL = "https://whatsapp-bot-go3u.onrender.com";
 
-        var chatContainer = document.createElement('div');
-        chatContainer.innerHTML = `
+        const BOT_WAITING = `Bot is processing your request. Please wait...`
+        var g = d.createElement(t),
+            s = d.getElementsByTagName(t)[0];
+        g.src = BASE_URL + "/socket.io/socket.io.js";
+        g.defer = true;
+        g.async = true;
+        s.parentNode.insertBefore(g, s);
+
+        var isToggle = false;
+        g.onload = function() {
+                var socket = io(BASE_URL);
+
+
+                var chatContainer = document.createElement('div');
+                chatContainer.innerHTML = `
           <div class="body">
             <button class="chatbot-toggler" onclick="handleToggle()">
-              <span class="toggle-icon">${isToggle ? '❌' : '💬'}</span>
+              <span class="toggle-icon">${isToggle ? '❌' : '<i class="bi bi-chat-fill"></i>'}</span>
               <span class="material-symbols-outlined">close</span>
             </button>
             <div id="chat-container" class=${isToggle ? 'chatbot show-chatbot' : 'chatbot'}>
               <header>
-                <h2>Chatbot</h2>
+                <h2>JerryBot</h2>
                 <span class="close-btn material-symbols-outlined" onclick="handleToggle()">
                   close
                 </span>
               </header>
               <ul class="chatbox" id="messages"></ul>
               <div class="chat-input">
-              <form id="form" action="">
-              <input id="m" autocomplete="off" />
-              <button onclick="handleSendMessage()">Send</button>
-            </form>
+              <textarea placeholder="Enter a message..." spellcheck="false" required></textarea>
+                <span id="send-btn" class="material-symbols-rounded">send</span>
               </div>
             </div>
           </div>
       `;
-        var toggleChatButton = document.createElement('button');
-        document.body.appendChild(toggleChatButton);
-        document.body.appendChild(chatContainer);
 
-        // Code to handle user input and display messages
-        const form = document.getElementById('form');
-        const input = document.getElementById('m');
-        const messages = document.getElementById('messages');
+                console.log(isToggle)
+                var toggleChatButton = document.createElement('button');
+                document.body.appendChild(toggleChatButton);
+                document.body.appendChild(chatContainer);
 
-        window.handleToggle = function() {
-            const chatContainer = document.getElementById('chat-container');
-            if (!chatContainer) {
-                console.error("Chat container not found");
-                return;
-            }
+                // Code to handle user input and display messages
+                const chatbox = document.querySelector(".chatbox");
+                const chatInput = document.querySelector(".chat-input textarea");
+                const sendChatBtn = document.querySelector(".chat-input span");
+                const inputInitHeight = chatInput.scrollHeight;
 
-            const isChatVisible = chatContainer.classList.contains('show-chatbot');
+                window.handleToggle = function() {
+                    const chatContainer = document.getElementById('chat-container');
+                    if (!chatContainer) {
+                        console.error("Chat container not found");
+                        return;
+                    }
+                    const isChatVisible = chatContainer.classList.contains('show-chatbot');
+                    if (isChatVisible) {
+                        chatContainer.classList.remove('show-chatbot');
+                        isToggle = false
+                    } else {
+                        chatContainer.classList.add('show-chatbot');
+                        isToggle = true
 
-            if (isChatVisible) {
-                chatContainer.classList.remove('show-chatbot');
-            } else {
-                chatContainer.classList.add('show-chatbot');
 
-            }
-        };
-
-        form.addEventListener('submit', function(e) {
-            e.preventDefault();
-            const message = input.value.trim();
-
-            if (message !== '') {
-                const newMessage = {
-                    user: '',
-                    message: message,
-                    isOwnMsg: true,
+                    }
                 };
-                socket.emit('sendMessage', newMessage);
-                input.value = '';
-            }
 
-            return false;
-        });
+
+
+
+                function handleChat() {
+                    const message = chatInput.value.trim();
+                    if (message !== '') {
+                        const newMessage = {
+                            user: '',
+                            message: message,
+                            isOwnMsg: true,
+                        };
+
+                        chatInput.style.height = `${inputInitHeight}px`;
+                        chatbox.appendChild(createChatLi(message, "outgoing"));
+                        chatbox.scrollTo(0, chatbox.scrollHeight);
+                        setTimeout(() => {
+                            chatbox.scrollTo(0, chatbox.scrollHeight);
+                        }, 0);
+                        socket.emit('sendMessage', newMessage);
+                        chatInput.value = '';
+                    }
+
+                    return false;
+                };
+
+                sendChatBtn.addEventListener("click", handleChat);
+
+                chatInput.addEventListener("input", () => {
+                    chatInput.style.height = `${inputInitHeight}px`;
+                    chatInput.style.height = `${chatInput.scrollHeight}px`;
+                });
+
+                chatInput.addEventListener("keydown", (e) => {
+                    if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+                        e.preventDefault();
+                        handleChat();
+                    }
+                });
+
+                const createChatLi = (message, className) => {
+                        // p.className = msg.message === BOT_WAITING ? "bg-success text-white" : "";
+
+                        const chatLi = document.createElement("li");
+                        chatLi.classList.add("chat", `${className}`);
+                        let chatContent = className === "outgoing" ? `<p></p>` :
+                            `<span class="material-symbols-outlined"><i class="bi bi-person-circle"></i></span><p ${message === `${BOT_WAITING}` ? "big-success" : ""}></p>`;
+            chatLi.innerHTML = chatContent;
+            chatLi.querySelector("p").textContent = message;
+            return chatLi;
+        }
 
         socket.on('chat-message', function(msg) {
-            console.log(msg);
-            const li = document.createElement('li');
-            li.className = `${msg.isOwnMsg ? 'chat outgoing' : 'chat incoming'}`;
+            chatbox.appendChild(createChatLi(msg.message, "incoming"));
+            chatbox.scrollTo(0, chatbox.scrollHeight);
 
-            const p = document.createElement('p');
-            p.className = msg.message === BOT_WAITING ? "bg-success text-white" : "";
+            setTimeout(() => {
+                chatbox.scrollTo(0, chatbox.scrollHeight);
+            }, 0);
 
-            p.appendChild(document.createTextNode(msg.message));
-
-            li.appendChild(p);
-            messages.appendChild(li);
         });
-
 
         socket.emit('setUserId', userId);
 
